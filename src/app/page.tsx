3c4d788e8218d35e2e749753c3419c9fcf0c58d6
AjayTeotia <teotiaajay20@gmail.com>
2025-07-19 16:30:35 +0530

    2.Login-Page-UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/theme/theme-toggle";
 
 export default function Home() {
   return (
     <div className="">
       <h1 className="text-3xl">Home Page</h1>
 
-      <Button>Click me</Button>
+      <ThemeToggle />
     </div>
   );
 }
