4ee9a588065c1197a95737d454d6b50042370146
AjayTeotia <teotiaajay20@gmail.com>
2025-07-26 16:29:59 +0530

    11.Upload-With-S3-And-Presigned-Urls

--- src/components/file-uploader/uploader.tsx
+++ src/components/file-uploader/uploader.tsx
@@ -31,11 +31,37 @@ export function Uploader() {
     fileType: "image",
   });
 
-  function uploadFile(file: File) {
+  async function uploadFile(file: File) {
     setFileState((prev) => ({ ...prev, uploading: true, progress: 0 }));
 
     try {
-    } catch (error) {}
+      const presignedResponse = await fetch("/api/s3/upload", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          fileName: file.name,
+          contentType: file.type,
+          size: file.size,
+          isImage: true,
+        }),
+      });
+
+      if (!presignedResponse.ok) {
+        toast.error("Failed to upload file. Please try again.");
+        setFileState((prev) => ({
+          ...prev,
+          uploading: false,
+          progress: 0,
+          error: true,
+        }));
+
+        return;
+      }
+
+      const { presignedUrl, key } = await presignedResponse.json();
+
+       
+    } catch {}
   }
 
   const onDrop = useCallback((acceptedFiles: File[]) => {
