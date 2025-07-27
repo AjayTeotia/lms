10f12e12333c7ad61efd1d04898390d207234f0a
AjayTeotia <teotiaajay20@gmail.com>
2025-07-27 16:20:45 +0530

    15.Protect-Endpoints

--- src/app/admin/course/create/action.ts
+++ src/app/admin/course/create/action.ts
@@ -1,18 +1,52 @@
 "use server";
 
-import { auth } from "@/lib/auth";
+import { requireUser } from "@/app/data/admin/require-admin";
+import arcjet, { detectBot, fixedWindow } from "@/lib/arcjet";
 import { prisma } from "@/lib/db";
 import { ApiResponse } from "@/lib/type";
 import { courseSchema, CourseSchemaType } from "@/lib/zod-schema";
-import { headers } from "next/headers";
+import { request } from "@arcjet/next";
+
+const aj = arcjet
+  .withRule(
+    detectBot({
+      mode: "LIVE",
+      allow: [],
+    })
+  )
+  .withRule(
+    fixedWindow({
+      mode: "LIVE",
+      window: "1m",
+      max: 5,
+    })
+  );
 
 export async function CreateCourse(
   data: CourseSchemaType
 ): Promise<ApiResponse> {
+  const session = await requireUser();
+
   try {
-    const session = await auth.api.getSession({
-      headers: await headers(),
+    const req = await request();
+
+    const decision = await aj.protect(req, {
+      fingerprints: session?.user.id as string,
     });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return {
+          status: "error",
+          message: "You have been blocked due to too many requests.",
+        };
+      } else {
+        return {
+          status: "error",
+          message: "You are bot! if you are not a bot, please try again.",
+        };
+      }
+    }
 
     const validation = courseSchema.safeParse(data);
 
