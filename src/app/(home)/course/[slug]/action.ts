52272717ba341057395880e99070a681a08bb065
AjayTeotia <teotiaajay20@gmail.com>
2025-08-02 14:18:27 +0530

    23.Implement-Enrollment(Stripe)

---
+++ src/app/(home)/course/[slug]/action.ts
@@ -0,0 +1,178 @@
+"use server";
+
+import { requireUser } from "@/app/data/user/require-user";
+import arcjet, { fixedWindow } from "@/lib/arcjet";
+import { prisma } from "@/lib/db";
+import { env } from "@/lib/env";
+import { stripe } from "@/lib/stripe";
+import { ApiResponse } from "@/lib/type";
+import { request } from "@arcjet/next";
+import { redirect } from "next/navigation";
+import Stripe from "stripe";
+
+const aj = arcjet.withRule(
+  fixedWindow({
+    mode: "LIVE",
+    window: "1m",
+    max: 5,
+  })
+);
+
+export async function enrollInCourseAction(
+  courseId: string
+): Promise<ApiResponse | never> {
+  const user = await requireUser();
+
+  let checkoutUrl: string;
+  try {
+    const req = await request();
+
+    const decision = await aj.protect(req, {
+      fingerprints: user.user.id,
+    });
+
+    if (decision.isDenied()) {
+      return {
+        status: "error",
+        message: "You have been blocked due to too many requests.",
+      };
+    }
+
+    const course = await prisma.course.findUnique({
+      where: {
+        id: courseId,
+      },
+      select: {
+        id: true,
+        title: true,
+        price: true,
+        slug: true,
+      },
+    });
+
+    if (!course) {
+      return {
+        status: "error",
+        message: "Course not found",
+      };
+    }
+
+    let stripeCustomerId: string;
+    const userWithStripeCustomerId = await prisma.user.findUnique({
+      where: {
+        id: user.user.id,
+      },
+      select: {
+        stripeCustomerId: true,
+      },
+    });
+
+    if (userWithStripeCustomerId?.stripeCustomerId) {
+      stripeCustomerId = userWithStripeCustomerId.stripeCustomerId;
+    } else {
+      const customer = await stripe.customers.create({
+        email: user.user.email,
+        name: user.user.name,
+        metadata: {
+          userId: user.user.id,
+        },
+      });
+
+      stripeCustomerId = customer.id;
+
+      await prisma.user.update({
+        where: {
+          id: user.user.id,
+        },
+        data: {
+          stripeCustomerId: stripeCustomerId,
+        },
+      });
+    }
+
+    const result = await prisma.$transaction(async (tx) => {
+      const existingEnrollment = await tx.enrollment.findUnique({
+        where: {
+          courseId_userId: {
+            userId: user.user.id,
+            courseId: course.id,
+          },
+        },
+        select: {
+          status: true,
+          id: true,
+        },
+      });
+
+      if (existingEnrollment?.status === "ACTIVE") {
+        return {
+          status: "success",
+          message: "Successfully enrolled in course",
+        };
+      }
+
+      let enrollment;
+
+      if (existingEnrollment) {
+        enrollment = await tx.enrollment.update({
+          where: {
+            id: existingEnrollment.id,
+          },
+          data: {
+            amount: course.price,
+            status: "PENDING",
+            updatedAt: new Date(),
+          },
+        });
+      } else {
+        enrollment = await tx.enrollment.create({
+          data: {
+            courseId: course.id,
+            userId: user.user.id,
+            amount: course.price,
+            status: "PENDING",
+          },
+        });
+      }
+
+      const checkoutSession = await stripe.checkout.sessions.create({
+        customer: stripeCustomerId,
+        line_items: [
+          {
+            price: "price_1RrLReBf2dCOnsiSjJhoq894",
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        success_url: `${env.BETTER_AUTH_URL}/payment/success`,
+        cancel_url: `${env.BETTER_AUTH_URL}/payment/cancel`,
+        metadata: {
+          userId: user.user.id,
+          courseId: course.id,
+          enrollmentId: enrollment.id,
+        },
+      });
+
+      return {
+        enrollment: enrollment,
+        checkoutUrl: checkoutSession.url,
+      };
+    });
+
+    checkoutUrl = result.checkoutUrl as string;
+  } catch (error) {
+    if (error instanceof Stripe.errors.StripeError) {
+      return {
+        status: "error",
+        message: "Payment system error. Please try again.",
+      };
+    }
+
+    return {
+      status: "error",
+      message: "Failed to enroll in course",
+    };
+  }
+
+  redirect(checkoutUrl);
+}
--- src/app/(home)/course/[slug]/page.tsx
+++ src/app/(home)/course/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { getIndividualCourse } from "@/app/data/course/get-course";
+import { checkIsCourseBought } from "@/app/data/user/user-is-enrolled";
 import { RenderDescription } from "@/components/rich-text-editor/render-description";
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import {
   Collapsible,
@@ -20,12 +20,15 @@ import {
 } from "@tabler/icons-react";
 import { CheckIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import { EnrollmentButton } from "./_components/enrollment-button";
 
 type Params = Promise<{ slug: string }>;
 
 export default async function SlugPage({ params }: { params: Params }) {
   const { slug } = await params;
   const course = await getIndividualCourse(slug);
+  const isEnrolled = await checkIsCourseBought(course.id);
 
   return (
     <div className="grid grid-cols-1 gap-8 lg:grid-cols-3 mt-5">
@@ -273,9 +276,15 @@ export default async function SlugPage({ params }: { params: Params }) {
                 </ul>
               </div>
 
-              <Button className="w-full">Enroll Now!</Button>
-             
-              <p className="mt-3 text-center text-xs text-muted-foreground">30-Day Money-Back Guarantee</p>
+              {isEnrolled ? (
+                <Link href="/dashboard">Watch Course</Link>
+              ) : (
+                <EnrollmentButton courseId={course.id} />
+              )}
+
+              <p className="mt-3 text-center text-xs text-muted-foreground">
+                30-Day Money-Back Guarantee
+              </p>
             </CardContent>
           </Card>
         </div>
