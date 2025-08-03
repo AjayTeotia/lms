3c5429002b8109d9992ed54d63d48d3cc19ead84
AjayTeotia <teotiaajay20@gmail.com>
2025-08-03 22:03:12 +0530

    26.Create-Enrollment-Dashboard

---
+++ src/app/dashboard/[slug]/page.tsx
@@ -0,0 +1,6 @@
+
+
+export default async function CourseSlugPage() {
+
+  return <div>CourseSlugPage</div>;
+}
---
+++ src/app/data/course/get-course-sidebar-data.ts
@@ -0,0 +1,64 @@
+import "server-only";
+
+import { requireUser } from "../user/require-user";
+import { prisma } from "@/lib/db";
+import { notFound } from "next/navigation";
+
+export async function getCourseSidebarData(slug: string) {
+  const session = await requireUser();
+
+  const course = await prisma.course.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      title: true,
+      fileKey: true,
+      duration: true,
+      level: true,
+      category: true,
+      slug: true,
+      chapter: {
+        orderBy: {
+          position: "asc",
+        },
+        select: {
+          id: true,
+          title: true,
+          position: true,
+          lesson: {
+            orderBy: {
+              position: "asc",
+            },
+            select: {
+              id: true,
+              title: true,
+              description: true,
+              position: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (!course) return notFound();
+
+  const enrollment = await prisma.enrollment.findUnique({
+    where: {
+      courseId_userId: {
+        userId: session.user.id,
+        courseId: course.id,
+      },
+    },
+  });
+
+  if (!enrollment || enrollment.status !== "ACTIVE") return notFound();
+
+  return course;
+}
+
+export type CourseSidebarDataType = Awaited<
+  ReturnType<typeof getCourseSidebarData>
+>;
